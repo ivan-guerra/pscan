71fb31f5bf2be4055b9c0721d9df10d242ced8ee
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2025-01-21 21:45:49 -0500

    Add support for hostname resolution.

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,51 @@
 use clap::Parser;
 use ping_rs::PingApiOutput;
 use scanners::{PortRange, Scan, ScanProtocol, TcpScanner, UdpScanner};
-use std::net::IpAddr;
+use std::fmt::{self, Display, Formatter};
+use std::net::{IpAddr, ToSocketAddrs};
+use std::str::FromStr;
 use std::time::Duration;
 
 mod results;
 mod scanners;
 
+#[derive(Debug, Clone)]
+enum Address {
+    Ip(IpAddr),
+    Hostname(String),
+}
+
+impl Display for Address {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match self {
+            Address::Ip(ip) => write!(f, "{}", ip),
+            Address::Hostname(hostname) => write!(f, "{}", hostname),
+        }
+    }
+}
+
+impl FromStr for Address {
+    type Err = String;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        if let Ok(ip) = input.parse::<IpAddr>() {
+            Ok(Address::Ip(ip))
+        } else {
+            Ok(Address::Hostname(input.to_string()))
+        }
+    }
+}
+
+fn parse_addr(input: &str) -> Result<Address, String> {
+    input.parse::<Address>()
+}
+
 #[doc(hidden)]
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    #[arg(help = "Target IP address")]
-    addr: std::net::IpAddr,
+    #[arg(value_parser = parse_addr, help = "Target IP address")]
+    addr: Address,
 
     #[arg(
         short,
@@ -50,9 +83,24 @@ fn ping_host(addr: &IpAddr) -> PingApiOutput {
     ping_rs::send_ping(addr, timeout, &data, Some(&options))
 }
 
+fn resolve_url_to_ip(hostname: &str) -> Option<IpAddr> {
+    let addr = format!("{}:0", hostname);
+    addr.to_socket_addrs()
+        .ok() // Attempt to resolve
+        .and_then(|mut iter| iter.next()) // Take the first resolved address
+        .map(|socket_addr| socket_addr.ip()) // Extract the IpAddr
+}
+
 #[doc(hidden)]
 fn run(args: Args) -> Result<(), Box<dyn std::error::Error>> {
-    let result = ping_host(&args.addr);
+    let addr = match args.addr {
+        Address::Ip(ip) => ip,
+        Address::Hostname(ref hostname) => {
+            resolve_url_to_ip(hostname).ok_or(format!("Could not resolve hostname {}", hostname))?
+        }
+    };
+
+    let result = ping_host(&addr);
     match result {
         Ok(reply) => println!("Host is up ({}ms latency).", reply.rtt),
         Err(e) => return Err(format!("Host is unreachable, {:?}", e).into()),
@@ -66,7 +114,7 @@ fn run(args: Args) -> Result<(), Box<dyn std::error::Error>> {
     };
     let scanner = get_scanner(&args.scan_protocol);
     let start_time = std::time::Instant::now();
-    let results = scanner.scan(&args.addr, &args.port_range);
+    let results = scanner.scan(&addr, &args.port_range);
     let duration = start_time.elapsed();
 
     results::print_results(&args, results, duration);
